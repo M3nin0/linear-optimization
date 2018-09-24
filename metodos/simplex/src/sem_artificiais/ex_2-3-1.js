81856ca3c0f3953bb938056c802d5ce3d63b6a71
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2018-09-23 23:05:55 -0300

    Adição do exercício 2.3.1 do livro de pesquisa operacional

---
+++ metodos/simplex/src/sem_artificiais/ex_2-3-1.js
@@ -0,0 +1,52 @@
+/*
+Exercício (2.3.1) do livro Pesquisa operacional na tomada de decisões.
+*/
+
+const glpk = require('../glpk.js');
+
+let lp = {
+  name: 'LP',
+  objective: {
+    direction: glpk.GLP_MAX,
+    name: 'obj',
+    vars: [
+      { name: 'x1', coef: 4.0 },
+      { name: 'x2', coef: 3.0 }
+    ]
+  },
+  subjectTo: [
+    {
+      name: 'cons1',
+      vars: [
+        { name: 'x1', coef: 1.0 },
+        { name: 'x2', coef: 3.0 }
+      ],
+      bnds: { type: glpk.GLP_UP, ub: 7.0, lb: 0.0 }
+    },
+    {
+      name: 'cons2',
+      vars: [
+        { name: 'x1', coef: 2.0 },
+        { name: 'x2', coef: 2.0 }
+      ],
+      bnds: { type: glpk.GLP_UP, ub: 8.0, lb: 0.0 }
+    },
+    {
+      name: 'cons3',
+      vars: [
+        {name: 'x1', coef: 1.0},
+        {name: 'x2', coef: 1.0}
+      ],
+      bnds: {type: glpk.GLP_UP, ub: 3.0, lb: 0.0}
+    },
+    {
+      name: 'cons4',
+      vars: [
+        {name: 'x2', coef: 1.0}
+      ],
+      bnds: {type: glpk.GLP_UP, ub: 2.0, lb: 0.0}
+    }
+  ]
+};
+
+console.log(glpk.solve(lp));
